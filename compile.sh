19abeedd4e29848ece88c51ebafaf1c7717ca3dd
q66 <daniel@octaforge.org>
2015-08-29 00:18:50 -0700

    simple build script

---
+++ compile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+CUBESCRIPT_PATH="../libcubescript"
+OCTASTD_PATH="../octastd"
+
+CXXFLAGS="-g"
+
+c++ main.cc $CUBESCRIPT_PATH/cubescript.cc -o main -std=c++11 -Wall -Wextra \
+-I. -I$CUBESCRIPT_PATH -I$OCTASTD_PATH -pthread $CXXFLAGS
